c05cb2de3630a3c253b6d42edf91cf8aa4d4b3af
herme <hermes7308@gmail.com>
2024-03-30 21:48:50 -0700

    Add audio RAVDESS

--- preprocess/extract_features.py
+++ preprocess/extract_features.py
@@ -10,9 +10,12 @@ from tqdm.auto import tqdm
 from marlin_pytorch import Marlin
 from marlin_pytorch.config import resolve_config
 from marlin_pytorch.util import get_mfccs, audio_load
+from model.audio_resnet.audio_resnet18 import AudioResNet18
+
 
 # Used to get speech xvector embeddings
-#from speechbrain.inference.speaker import EncoderClassifier
+# from speechbrain.inference.speaker import EncoderClassifier
+
 
 def delete_corrupted_files(filepath, corrupted_files):
     files = ["test.txt", "train.txt", "val.txt"]
@@ -25,17 +28,19 @@ def delete_corrupted_files(filepath, corrupted_files):
         with open(os.path.join(filepath, split), "w") as file:
             file.writelines(filtered_lines)
 
+
 def extract_audio(audio_path, audio_model, n_feats):
     audio, sr = audio_load(audio_path)
     audio_features = []
     for i in range(n_feats):
         start_idx = int(i * sr)
-        audio_buffer = audio[start_idx:start_idx+sr] # take 1sec audio windows
-        audio_feat = audio_model(audio_buffer) # compute embeddings using audio_model
+        audio_buffer = audio[start_idx:start_idx + sr]  # take 1sec audio windows
+        audio_feat = audio_model(audio_buffer)  # compute embeddings using audio_model
         audio_features.append(audio_feat)
     audio_features = [torch.from_numpy(arr).unsqueeze(0) for arr in audio_features]
     audio_features = torch.cat(audio_features, dim=0)
-    return audio_features # (n_feats, n_embedding)
+    return audio_features  # (n_feats, n_embedding)
+
 
 sys.path.append(".")
 
@@ -61,22 +66,25 @@ if __name__ == '__main__':
     video_model.cuda()
     video_model.eval()
 
+    # AudioResNet18
+
     # Audio embedding extractors
     if args.audio_backbone == "MFCC":
         audio_model = get_mfccs
     elif args.audio_backbone == "xvectors":
-        #audio_model = EncoderClassifier.from_hparams(source="speechbrain/spkrec-xvect-voxceleb", savedir="pretrained_models/spkrec-xvect-voxceleb")
+        # audio_model = EncoderClassifier.from_hparams(source="speechbrain/spkrec-xvect-voxceleb", savedir="pretrained_models/spkrec-xvect-voxceleb")
         pass
     elif args.audio_backbone == "resnet":
-        # TODO: Steve
-        raise ValueError(f"Error: {args.audio_backbone} not yet implemented")
+        audio_model = AudioResNet18()
+        audio_resnet_model_path = "../model/audio_resnet/RAVDESS_bs_32_lr_0.001_ep_100_03-30 21 21 49.pth"
+        audio_model.load_state_dict(torch.load(audio_resnet_model_path))
     elif args.audio_backbone == "emotion2vec":
         # TODO: Tom
         raise ValueError(f"Error: {args.audio_backbone} not yet implemented")
     else:
         raise ValueError(f"Incorrect audio_backbone {args.audio_backbone}")
     feat_dir_audio = args.audio_backbone
-    
+
     raw_video_path = os.path.join(args.data_dir, "cropped")
     raw_audio_path = os.path.join(args.data_dir, "audio")
     all_videos = sorted(list(filter(lambda x: x.endswith(".mp4"), os.listdir(raw_video_path))))
@@ -98,16 +106,17 @@ if __name__ == '__main__':
             np.save(save_path, video_embeddings.cpu().numpy())
             # save audio embeddings
             audio_embeddings = extract_audio(audio_path, audio_model, video_embeddings.shape[0])
-            assert audio_embeddings.shape[0] == video_embeddings.shape[0], "Video and audio n_feats dimension do not match"
+            assert audio_embeddings.shape[0] == video_embeddings.shape[
+                0], "Video and audio n_feats dimension do not match"
             audio_save_path = os.path.join(args.data_dir, feat_dir_audio, video_name.replace(".mp4", ".npy"))
             np.save(audio_save_path, audio_embeddings.cpu().numpy())
 
         except Exception as e:
             print(f"Video {video_path} error.", e)
             corrupted_files.append(video_name[:-4])
-            #feat = torch.zeros(0, video_model.encoder.embed_dim, dtype=torch.float32)
-            #audio_feat = torch.zeros(10, 87, dtype=torch.float32)
+            # feat = torch.zeros(0, video_model.encoder.embed_dim, dtype=torch.float32)
+            # audio_feat = torch.zeros(10, 87, dtype=torch.float32)
             continue
-    
+
     delete_corrupted_files(args.data_dir, corrupted_files)
     print(f"Files Corrupted and ignored: {len(corrupted_files)}")
