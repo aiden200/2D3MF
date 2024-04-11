52c4a38bea3c18cd7c46617e939b16f77bd3cd76
Adrian Roman <romanguz@discovery2.hpc.usc.edu>
2024-04-10 23:17:59 -0700

    integrate resnet embeddings in classifier

--- model/classifier.py
+++ model/classifier.py
@@ -90,7 +90,7 @@ class TD3MF(LightningModule):
             pass
         elif audio_backbone == "resnet":
             #TODO: Set self.audio_hidden_layers to the correct dimension
-            pass
+            self.audio_hidden_layers = 512
         else:
             raise ValueError("Unsupported audio backbone: Must be one of (MFCC, eat, xvectors, emotion2vec, resnet)")
         # add resenet and stuff
