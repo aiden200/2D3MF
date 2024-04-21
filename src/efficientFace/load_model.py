9dcbe988ef2c67e7dbe744a0bda8bfe201bcbde5
Aiden Chang <aidenchang@gmail.com>
2024-04-20 17:20:58 -0700

    efficientFace code added init

---
+++ src/efficientFace/load_model.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Parts of this code are based on https://github.com/zengqunzhao/EfficientFace/blob/master/models/EfficientFace.py
+"""
+
+import torch
+import torch.nn as nn
+from src.efficientFace.modulator import Modulator
+from src.efficientFace.efficientface import LocalFeatureExtractor, InvertedResidual
+
+def conv1d_block(in_channels, out_channels, kernel_size=3, stride=1, padding='same'):
+    return nn.Sequential(nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size,stride=stride, padding=padding),nn.BatchNorm1d(out_channels),
+                                   nn.ReLU(inplace=True)) 
+
+class EfficientFaceTemporal(nn.Module):
+
+    def __init__(self, stages_repeats, stages_out_channels, num_classes=7, im_per_sample=25):
+        super(EfficientFaceTemporal, self).__init__()
+
+        if len(stages_repeats) != 3:
+            raise ValueError('expected stages_repeats as list of 3 positive ints')
+        if len(stages_out_channels) != 5:
+            raise ValueError('expected stages_out_channels as list of 5 positive ints')
+        self._stage_out_channels = stages_out_channels
+
+        input_channels = 3
+        output_channels = self._stage_out_channels[0]
+        self.conv1 = nn.Sequential(nn.Conv2d(input_channels, output_channels, 3, 2, 1, bias=False),
+                                   nn.BatchNorm2d(output_channels),
+                                   nn.ReLU(inplace=True),)
+        input_channels = output_channels
+
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        stage_names = ['stage{}'.format(i) for i in [2, 3, 4]]
+        for name, repeats, output_channels in zip(stage_names, stages_repeats, self._stage_out_channels[1:]):
+            seq = [InvertedResidual(input_channels, output_channels, 2)]
+            for i in range(repeats - 1):
+                seq.append(InvertedResidual(output_channels, output_channels, 1))
+            setattr(self, name, nn.Sequential(*seq))
+            input_channels = output_channels
+
+        self.local = LocalFeatureExtractor(29, 116, 1)
+        self.modulator = Modulator(116)
+
+        output_channels = self._stage_out_channels[-1]
+
+        self.conv5 = nn.Sequential(nn.Conv2d(input_channels, output_channels, 1, 1, 0, bias=False),
+                                   nn.BatchNorm2d(output_channels),
+                                   nn.ReLU(inplace=True),)
+        self.conv1d_0 = conv1d_block(output_channels, 64)
+        self.conv1d_1 = conv1d_block(64, 64)
+        self.conv1d_2 = conv1d_block(64, 128)
+        self.conv1d_3 = conv1d_block(128, 128)
+
+        self.classifier_1 = nn.Sequential(
+                nn.Linear(128, num_classes),
+            )
+        self.im_per_sample = im_per_sample
+        
+    def forward_features(self, x):
+        x = self.conv1(x)
+        x = self.maxpool(x)
+        x = self.modulator(self.stage2(x)) + self.local(x)
+        x = self.stage3(x)
+        x = self.stage4(x)
+        x = self.conv5(x)
+        x = x.mean([2, 3]) #global average pooling
+        return x
+
+    def forward_stage1(self, x):
+        #Getting samples per batch
+        assert x.shape[0] % self.im_per_sample == 0, "Batch size is not a multiple of sequence length."
+        n_samples = x.shape[0] // self.im_per_sample
+        x = x.view(n_samples, self.im_per_sample, x.shape[1])
+        x = x.permute(0,2,1)
+        x = self.conv1d_0(x)
+        x = self.conv1d_1(x)
+        return x
+        
+        
+    def forward_stage2(self, x):
+        x = self.conv1d_2(x)
+        x = self.conv1d_3(x)
+        return x
+    
+    def forward_classifier(self, x):
+        x = x.mean([-1]) #pooling accross temporal dimension
+        x1 = self.classifier_1(x)
+        return x1
+    
+    def forward(self, x):
+        x = self.forward_features(x)
+        x = self.forward_stage1(x)
+        x = self.forward_stage2(x)
+        x = self.forward_classifier(x)
+        return x
+        
+      
+
+def init_feature_extractor(model, path):
+    if path == 'None' or path is None:
+        return
+    checkpoint = torch.load(path, map_location=torch.device('cpu'))
+    pre_trained_dict = checkpoint['state_dict']
+    pre_trained_dict = {key.replace("module.", ""): value for key, value in pre_trained_dict.items()}
+    print('Initializing efficientnet')
+    model.load_state_dict(pre_trained_dict, strict=False)
+
+    
+def get_model(num_classes, task, seq_length):
+    model = EfficientFaceTemporal([4, 8, 4], [29, 116, 232, 464, 1024], num_classes, task, seq_length)
+    return model  
+
+
+
+
+
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
